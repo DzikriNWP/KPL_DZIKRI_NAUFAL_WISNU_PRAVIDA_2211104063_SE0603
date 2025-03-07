1957d844eb7f3d5ace2065b7c0efd0a657122add
DzikriNWP <dzikrinaufal55@gmail.com>
2025-03-07 08:27:20 +0700

    First commit

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,8 @@
+let globalVariable = "Dzikri Naufal Wisnu Pravida";
+
+function getMyIdentify() {
+    let localVariable = "2211104063"
+    console.log(`Nama saya ${globalVariable} dan NIM saya ${localVariable}`);
+}
+console.log(globalVariable);
+getMyIdentify();
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,18 @@
+// var
+
+var name = "Dzikri Naufal Wisnu Pravida";
+const nim  = 2211104063
+
+console.log(name);
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log("Nama saya adalah ${name} dan nim saya adalah ${nim}")
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Dzikri Naufal Wisnu Pravida"
+
